800c4af835a98ec5867773f81849f5bf44cc1099
refmcris <cristianmartineztez@gmail.com>
2025-02-25 10:14:32 -0500

    adding dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { PrimeReactProvider } from "primereact/api";
+import "primereact/resources/themes/lara-light-blue/theme.css"; // Tema de PrimeReact
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <PrimeReactProvider>
+      <App />
+      
+    </PrimeReactProvider>
+  </StrictMode>
 )
