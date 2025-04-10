6f7ad34c6916c8fdcb5a39d921c38458312e625a
1Orchestratooor1 <rodriguezjhon2090@gmail.com>
2025-04-09 21:15:56 -0500

    merge resolving conflicts with main

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,3 +1,5 @@
 export * from "./user/header";
 export * from "./equipment/add-loan-dialog";
-
+export * from "./private-screen";
+export * from "./login-screen";
+export * from "./register-screen";
